a9d8d5a0f8b120ea423c3b61b5cff8feccdd6fb2
unknown <mudits02@gmail.com>
2022-02-24 23:07:45 +0530

    EDA Finished

--- EDA.py
+++ EDA.py
@@ -1,23 +1,24 @@
 import os
 import glob
+import sklearn as sk
 from sklearn.model_selection import train_test_split
 import shutil
 
 
-def split_data(path_to_data, path_to_save_train, path_to_save_valid, split_size=0.15):
+def split_data(path_to_data, path_to_save_train, path_to_save_valid, split_size=0.1):
 
     folders = os.listdir(path_to_data)
 
     for folder in folders:
 
         full_path = os.path.join(path_to_data, folder)
-        
-        images_path = glob.glob(os.path.join(full_path, '*.png'))
         # importing only .png files(images)
-        x_train, x_val = train_test_split(images_path, split_size)
+        images_path = glob.glob(os.path.join(full_path, '*.png'))
+
+        x_train, x_val = train_test_split(images_path, test_size=split_size)
 
         for x in x_train:
-            ##basename = os.path.basename(x)
+            #basename = os.path.basename(x)
             path_to_folder = os.path.join(path_to_save_train, folder)
 
             if not os.path.isdir(path_to_folder):
@@ -33,7 +34,10 @@ def split_data(path_to_data, path_to_save_train, path_to_save_valid, split_size=
 
             shutil.copy(x, path_to_folder)
 
-print("Bhadwa madarchod")
 
-        
-
+if __name__ == "__main__":
+    path_to_data = "D:\\Minor Project\\archive\\Train"
+    path_to_save_train = "D:\\Minor Project\\archive\\EDA_Train\\Train"
+    path_to_save_valid = "D:\\Minor Project\\archive\\EDA_Train\\Val"
+    split_data(path_to_data=path_to_data, path_to_save_train=path_to_save_train,
+               path_to_save_valid=path_to_save_valid)
